1c351d7fe12ee57e5619921ee5585bbae22b5be6
Chetan Patil <chetanpatil49554@gmail.com>
2020-01-16 22:54:01 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=18OrUPo7GcSLhJqfbH2H4R-nrv5yJuE6b'
-export_file_name = 'face6.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&open?id=1qB6D4mnS4MgU93te845TItm3IeNns_qq'
+export_file_name = 'face5.pkl'
 
-classes = ['ben_afflek','chetan_patil','elton_john','jerry_seinfeld','madonna','mindy_kaling']
+classes = ['tower','cathedral','maidan']
 path = Path(__file__).parent
 
 app = Starlette()
